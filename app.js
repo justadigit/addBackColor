2e5ae3a6cbe9d66001ec4ce6191d6dfd7be02ec3
justadigit <letmecode123456@gmail.com>
2020-08-07 21:11:27 +0630

    Changing Style

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const addbg = document.querySelector('.addbg');
-const bgcolor = Array.from(document.getElementsByClassName('bgcolor'));
-let i = 0;
-addbg.addEventListener('click', () => {
-  bgcolor[i++].style.backgroundColor = 'yellow';
-});
-
 // const addbg = document.querySelector('.addbg');
 // const bgcolor = Array.from(document.getElementsByClassName('bgcolor'));
-// const colorArr = ['red', 'green', 'blue', 'yellow'];
 // let i = 0;
-// let y = 0;
 // addbg.addEventListener('click', () => {
-//   bgcolor[i++].style.backgroundColor = colorArr[y++];
+//   bgcolor[i++].style.backgroundColor = 'yellow';
 // });
+
+const addbg = document.querySelector('.addbg');
+const bgcolor = Array.from(document.getElementsByClassName('bgcolor'));
+const colorArr = ['red', 'green', 'blue', 'yellow'];
+let i = 0;
+let y = 0;
+addbg.addEventListener('click', () => {
+  bgcolor[i++].style.backgroundColor = colorArr[y++];
+});
